9bab5afc3ca5e73ab84351e85d554ff317e99ab6
simyoju <dawnofspring@daum.net>
2021-04-20 01:09:57 +0900

    BOJ 2751 - 스파르탄365

---
+++ BOJ/Spartan/2751_sorting_number.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+num = []
+sorted_num = []
+for _ in range(n):
+    data = int(input())
+    num.append(data)
+
+sorted_num = sorted(num)
+
+for n in sorted_num:
+    print(n)
+    # print()
