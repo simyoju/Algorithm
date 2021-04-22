f0d15a97554a55afe5082d6dcc13155e5608ce42
simyoju <dawnofspring@daum.net>
2021-04-23 01:57:14 +0900

    BOJ 17298 스택을 인덱스 저장소로 활요하여 맞은 코드

--- BOJ/Spartan/17298_NGE.py
+++ BOJ/Spartan/17298_NGE.py
@@ -4,23 +4,29 @@
 ## 배열 탐색 중에 먼저 마주치는 Ai보다 큰수
 # (예외처리) 배열을 다 돌 때까지 그런 수가 없으면 -1 리턴
 
-
 import sys
-arr_NGE = list()
-
-def NGE(arr):
-    for i in range(len(arr)):
-        std_i = arr[i]
-        for j in range(i, len(arr)):
-            if std_i < arr[j]:
-                arr_NGE.append(arr[j])
-                break
-            elif j == (len(arr)-1):
-                arr_NGE.append(-1)
-
 N = int(sys.stdin.readline())
-arr_A = list(map(int ,sys.stdin.readline().split()))
-# for _ in range(N):
-NGE(arr_A)
-for a in arr_NGE:
-    print(a, end=" ")
+nums = list(map(int, sys.stdin.readline().split()))
+
+stck = []
+# 깨달은 부분 : 오큰수를 찾지 못하는 예외 케이슬 둘 것이 아니라,
+# 미리 -1 로 다 저장해놓으면 그것이 바로 예외 처리
+result = [-1 for _ in range(N)]
+
+stck.append(0)
+i = 1
+
+# 배운 부분: 스택에 값이 있고 while stck,
+# 인덱스도 사용하려면 and 명령어를 통해 i < N을 써주면 된다.
+while stck and i < N:
+    # 이해 안가는 부분: 왜 인덱스로 해야하는지 이해가 안감 -_-
+    while stck and nums[stck[-1]] < nums[i]:
+        print('stck[-1] is ', stck[-1])
+        result[stck[-1]] = nums[i]
+        stck.pop()
+
+    stck.append(i)
+    i += 1
+
+for i in result:
+    print(i, end=" ")
