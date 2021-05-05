63fc438aa00d8cc366af775135ad90d7e6cd98ab
simyoju <dawnofspring@daum.net>
2021-05-06 02:11:17 +0900

    BOJ 1932 - DP

--- BOJ/Spartan/3week_organic_cabbage.py
+++ BOJ/Spartan/3week_organic_cabbage.py
@@ -9,37 +9,42 @@ def bfs(x, y):
 
     que = deque()
     que.append((x, y))
+    # print(que)
 
-    plot[x][y] = 0
+    # plot[x][y] = 0
+    visited[x][y] = True
 
     while que:
         n = que.popleft()
+        # print('n: ', n)
 
         for i in range(4):
             nx = n[0] + directions[i][0]
             ny = n[1] + directions[i][1]
 
-            if (0<=nx<N) and (0<=ny<M):
-                if plot[nx][ny] == 1:
-                    plot[nx][ny] = 0
+            if (0<=nx<M) and (0<=ny<N):
+                if plot[nx][ny] == 1 and visited[nx][ny] == False:
+                    visited[nx][ny] = True
                     que.append((nx, ny))
 
 for _ in range(test_case):
     M, N, K = map(int, input().split())
     # print(M, N, K)
-    plot = [[0]*51 for _ in range(51)]
+    plot = [[[0]*M] for _ in range(N)]
+    visited = [[False]*M for _ in range(N)]
     cnt = 0
 
     for _ in range(K):
         x, y = map(int, input().split())
         # print(x, y)
-        plot[x][y] = 1
+        plot[y][x] = 1
 
     # print(plot)
     for i in range(N):
         for j in range(M):
-            if plot[i][j] == 1:
+            if plot[i][j] == 1 and visited:
+                # print('i, j: ', i, j)
                 cnt += 1
-                bfs(i, j)
-
+                bfs(i, j, visited)
+    # print(plot)
     print(cnt)
