6225cf69f62de570e42da2b56f0ac9c906db56ab
simyoju <dawnofspring@daum.net>
2021-05-08 04:53:28 +0900

    BOJ 2504 - 실패

--- BOJ/Spartan/4week_1912_sequence_sum.py
+++ BOJ/Spartan/4week_1912_sequence_sum.py
@@ -2,23 +2,10 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-arr = list(input().split())
-dp = [0 for _ in range(n)]
-ans = [0 for _ in range(n)]
+arr = list(map(int, input().split()))
+ans = [arr[0]]
 
-for i in range(n):
-    arr[i] = int(arr[i])
+for i in range(n-1):
+    ans.append(max(ans[i] + arr[i+1], arr[i+1]))
 
-for i in range(1, n):
-    dp[i-1] = arr[i-1]
-    for j in range(i, n):
-        cur = dp[j-1] + arr[j]
-        print(cur, j)
-        if cur < arr[j]:
-            ans[i-1] = dp[j-1]
-            break
-        dp[j] = cur
-    dp = [0 for _ in range(n)]
-
-print(ans)
 print(max(ans))
