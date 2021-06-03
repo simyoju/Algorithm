75de87257d44c15ef3f1f3b4a4543f1e25ec69ac
simyoju <dawnofspring@daum.net>
2021-06-03 13:15:10 +0900

    정답
    https://wackylife.tistory.com/22

--- Programmers/Level1/Find_Prime.py
+++ Programmers/Level1/Find_Prime.py
@@ -1,17 +1,9 @@
-def isPrime(n):
-    for i in range(2, n+1):
-        if i*i <= n:
-            if n%i == 0:
-                return False
-    return True
-
-
 def solution(n):
-    answer = 0
+    answer = set(range(2, n+1))
+
     for i in range(2, n+1):
-        if isPrime(i) == True:
-            answer += 1
-            #print(i)
-    return answer
+        if i in answer:
+            answer -= set(range(2*i, n+1, i))
+    return len(answer)
 
 print(solution(n=10))
