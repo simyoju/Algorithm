22c5cafa87118cfe92e0741ea5a8dfa63d54b773
simyoju <dawnofspring@daum.net>
2021-06-09 17:18:18 +0900

    정답!

--- Programmers/Level1/sum_two_numbers.py
+++ Programmers/Level1/sum_two_numbers.py
@@ -3,9 +3,10 @@ def solution(numbers):
     for i in range(len(numbers)):
         for j in range(i+1, len(numbers)):
             answer.append(numbers[i] + numbers[j])
-    answer.sort()
     answer = list(set(answer))
+    answer.sort()
     return answer
 
 #print(solution(numbers=[2,1,3,4,1]))
-print(solution(numbers=[5,0,2,7]))
+#print(solution(numbers=[5,0,2,7]))
+print(solution(numbers=[1,100,100,100,100,100,100,99]))
